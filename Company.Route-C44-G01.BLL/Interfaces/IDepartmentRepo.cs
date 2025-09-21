818bfd3e7cc031823ffc3a72961daaa15626a71c
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-09-22 00:24:23 +0300

    Department Module BLL

---
+++ Company.Route-C44-G01.BLL/Interfaces/IDepartmentRepo.cs
@@ -0,0 +1,20 @@
+﻿using Company.Route_C44_G01.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route_C44_G01.BLL.Interfaces
+{
+    public interface IDepartmentRepo
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int id);
+
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
