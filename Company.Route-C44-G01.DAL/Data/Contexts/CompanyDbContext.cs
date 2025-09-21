93d5de6b293e0b02f46d8cfc53bad056c0dcc9fe
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-09-21 23:02:17 +0300

    Implementation Of DAL

---
+++ Company.Route-C44-G01.DAL/Data/Contexts/CompanyDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Company.Route_C44_G01.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Route_C44_G01.DAL.Data.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext() : base()
+        {
+
+        }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = .; Database = Company.RouteG01; Trusted_Connection = True; TrustServerCertificate = True");
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
