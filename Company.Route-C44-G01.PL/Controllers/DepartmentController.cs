fc9baed29f558fe2828bffbc309b00abcca7423d
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-09-22 01:36:54 +0300

    Create Department Controller And Enable DI For All Services

---
+++ Company.Route-C44-G01.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Company.Route_C44_G01.BLL.Interfaces;
+using Company.Route_C44_G01.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Route_C44_G01.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepo _departmentRepository;
+
+        public DepartmentController(IDepartmentRepo departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        [HttpGet]  // GET: /Department/Index 
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+
+            return View(departments);
+        }
+    }
+}
--- Company.Route-C44-G01.PL/Program.cs
+++ Company.Route-C44-G01.PL/Program.cs
@@ -1,3 +1,8 @@
+using Company.Route_C44_G01.BLL.Interfaces;
+using Company.Route_C44_G01.BLL.Repositories;
+using Company.Route_C44_G01.DAL.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Route_C44_G01.PL
 {
     public class Program
@@ -7,7 +12,12 @@ namespace Company.Route_C44_G01.PL
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews(); // Add MVC services to the container
+            builder.Services.AddScoped<IDepartmentRepo,DepartmentRepository>(); // Register DepartmentRepository for dependency injection
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            }); // Register CompanyDbContext for dependency injection
 
             var app = builder.Build();
 
