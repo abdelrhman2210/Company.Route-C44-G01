6ed21809db9283cf4dd2eede9b7802d8d94f609e
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-09-21 22:21:22 +0300

    Add project files.

---
+++ Company.Route-C44-G01.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Company.Route_C44_G01.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
