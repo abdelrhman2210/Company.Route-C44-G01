00b2f8343775868d380538be7ee126a45d8abeae
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-10-15 21:09:51 +0300

    Identity Package - DAL Implementation (Session07-V02+V03)

--- Company.Route-C44-G01.DAL/Data/Contexts/CompanyDbContext.cs
+++ Company.Route-C44-G01.DAL/Data/Contexts/CompanyDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Company.Route_C44_G01.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Company.Route_C44_G01.DAL.Data.Contexts
 {
-    public class CompanyDbContext : DbContext
+    public class CompanyDbContext : IdentityDbContext<AppUser>
     {
         public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options)
         {
@@ -30,5 +32,8 @@ namespace Company.Route_C44_G01.DAL.Data.Contexts
 
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
-        }
+        //public DbSet<IdentityUser> Users { get; set; }
+        //public DbSet<IdentityRole> Roles { get; set; }
+
+    }
 }
