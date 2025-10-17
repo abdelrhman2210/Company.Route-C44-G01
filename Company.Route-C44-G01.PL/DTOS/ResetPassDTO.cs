733a493231aa320d2f435f854876777f9835afaf
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-10-17 10:28:10 +0300

    Reset Password Implementation

---
+++ Company.Route-C44-G01.PL/DTOS/ResetPassDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Route_C44_G01.PL.DTOS
+{
+    public class ResetPassDTO
+    {
+        [Required(ErrorMessage = "Password is Required !!")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+        [Required(ErrorMessage = "Confirm Password is Required !!")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword), ErrorMessage = "Password and Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
