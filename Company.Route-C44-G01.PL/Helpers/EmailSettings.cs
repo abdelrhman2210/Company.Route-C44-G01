3264296b82b313a504e12f9b40b30c6cf154d343
abdelrhman2210 <abdelrhmanessam2210@gmail.com>
2025-10-17 10:00:35 +0300

    Implementation Of ForgetPassword + SendEmail

---
+++ Company.Route-C44-G01.PL/Helpers/EmailSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace Company.Route_C44_G01.PL.Helpers
+{
+    public static class EmailSettings
+    {
+        public static bool SendEmail(Email email)
+        {
+            try
+            {
+                var smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.EnableSsl = true;
+                //sfrdsardabdwwjiv
+                smtpClient.Credentials = new NetworkCredential("abdelrhmanessam2210@gmail.com", "sfrdsardabdwwjiv");
+                smtpClient.Send("abdelrhmanessam2210@gmail.com", email.To, email.Subject, email.Body);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
